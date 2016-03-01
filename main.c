5301485f6e454104c5fe1fb76ca8f72ab2635ef2
io12 <io12@users.noreply.github.com>
2016-02-29 20:33:44 -0500

    added colored walls (pretty much broken)

--- main.c
+++ main.c
@@ -3,26 +3,25 @@
 
 #define PI 3.14159265
 #define RJMP 0.01
-#define WALLFG (char) 219
 #define WALLMG '#'
 #define WALLBG ':'
 
 int main() {
-	double pAngle = 0;
-	double pX = 2;
-	double pY = 2;
-	double rAngle;
+	double pAngle = 0, rAngle;
+	double pX = 2, pY=2;
 	double rX, rY;
 	double fov = 2;
 	double dist;
 	double mvfactor = 0.1;
 	int rows, cols;
+	// used for wall color and presence
+	int wStatus;
 	int map[10][10] = {
-		{1,1,1,1,1,1,1,1,1,1},
+		{1,1,2,1,3,1,4,1,5,1},
 		{1,0,0,0,0,0,0,0,0,1},
+		{1,0,0,0,0,0,0,0,0,6},
 		{1,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,0,0,0,0,0,1},
+		{1,0,0,0,0,0,0,0,0,7},
 		{1,0,0,0,0,0,0,0,0,1},
 		{1,0,0,0,0,0,0,0,0,1},
 		{1,0,0,0,0,0,0,0,0,1},
@@ -34,6 +33,10 @@ int main() {
 	cbreak();
 	keypad(stdscr, 1);
 	curs_set(0);
+	start_color();
+	init_color(COLOR_BLACK, 0, 0, 0);
+	init_pair(1, COLOR_WHITE, COLOR_BLACK);
+	attron(COLOR_PAIR(1));
 	// gameloop starts here
 	while(1) {
 		getmaxyx(stdscr, rows, cols);
@@ -49,7 +52,8 @@ int main() {
 				rY += sin(rAngle) * RJMP;
 				dist += RJMP;
 				// check if ray hit a wall
-				if (map[(int) rY][(int) rX])
+				wStatus = map[(int) rY][(int) rX];
+				if (wStatus)
 					break;
 			}
 			// create a column
@@ -59,11 +63,48 @@ int main() {
 			// this may be problematic if (rows - h) is not even
 			int f = ( rows - h ) / 2;
 			for (int ir = 0; ir < rows; ir++) {
-				// I have no idea if I even did this part right
-				(ir < f) || (ir > (f + h)) ? mvaddch(ir, ic, ' ') :
-				dist < 2 ? mvaddch(ir, ic, WALLFG) :
-				dist < 4 ? mvaddch(ir, ic, WALLMG) :
-				mvaddch(ir, ic, WALLBG);
+				// draw the walls
+				switch (wStatus) {
+					case 2:
+						init_pair(1, COLOR_RED, COLOR_BLACK);
+						attron(COLOR_PAIR(1));
+						break;
+					case 3:
+						init_pair(1, COLOR_GREEN, COLOR_BLACK);
+						attron(COLOR_PAIR(1));
+						break;
+					case 4:
+						init_pair(1, COLOR_YELLOW, COLOR_BLACK);
+						attron(COLOR_PAIR(1));
+						break;
+					case 5:
+						init_pair(1, COLOR_BLUE, COLOR_BLACK);
+						attron(COLOR_PAIR(1));
+						break;
+					case 6:
+						init_pair(1, COLOR_MAGENTA, COLOR_BLACK);
+						attron(COLOR_PAIR(1));
+						break;
+					case 7:
+						init_pair(1, COLOR_CYAN, COLOR_BLACK);
+						attron(COLOR_PAIR(1));
+						break;
+					default:
+						break;
+				}
+				if ((ir < f) || (ir > (f + h))) {
+					mvaddch(ir, ic, ' ');
+				}
+				else if (dist < 2) {
+					attron(A_STANDOUT);
+					mvaddch(ir, ic, ' ');
+					attroff(A_STANDOUT);
+				}
+				else if (dist < 4)
+					mvaddch(ir, ic, WALLMG);
+				else
+					mvaddch(ir, ic, WALLBG);
+				attroff(COLOR_PAIR(1));
 			}
 		}
 		// print the frame
