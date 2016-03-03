1e116592f18c8fa9e3c9c59aa8eb5ac52f7ae97a
io12 <io12@users.noreply.github.com>
2016-03-02 19:02:04 -0500

    cleaned up code

--- main.c
+++ main.c
@@ -1,30 +1,55 @@
 #include <math.h>
 #include <curses.h>
 
-#define RAY_JUMP_FACTOR 0.1
-#define WALL_DARK '#'
-#define WALL_DARKER ':'
-#define PLAYER_MOVE_FACTOR 0.1
-#define FOV 2.0
+#include "main.h"
+
+double fov = 2;
+double pX = 2, pY = 2;
+double pAngle = 0;
 
 int main() {
-	double pAngle = 0, rAngle;
-	double pX = 2, pY = 2, rX, rY;
-	double rDist;
-	// used for wall color and presence
-	int wStatus;
-	int map[10][10] = {
-		{1,1,2,1,3,1,4,1,5,1},
-		{1,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,0,0,0,0,0,6},
-		{1,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,0,0,0,0,0,7},
-		{1,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,0,0,0,0,0,1},
-		{1,1,1,1,1,1,1,1,1,1}
-	};
+	init_raycaster();
+	// gameloop starts here
+	while(1) {
+		gen_frame(pY, pX, pAngle, MAPLENGTH, map);
+		// print the frame
+		refresh();
+		// get input
+		switch (getch()) {
+			case KEY_UP:
+				// collision checking
+				if (!map[(int) (pY + sin(pAngle+0.5*M_PI) * P_MV_FACTOR)][(int) (pX + cos(pAngle+0.5*M_PI) * P_MV_FACTOR)]) {
+					pX += cos(pAngle+fov) * P_MV_FACTOR;
+					pY += sin(pAngle+fov) * P_MV_FACTOR;
+				}
+				break;
+			case KEY_DOWN:
+				if (!map[(int) (pY - sin(pAngle+0.5*M_PI) * P_MV_FACTOR)][(int) (pX - cos(pAngle+0.5*M_PI) * P_MV_FACTOR)]) {
+					pX -= cos(pAngle+fov) * P_MV_FACTOR;
+					pY -= sin(pAngle+fov) * P_MV_FACTOR;
+				}
+				break;
+			case KEY_RIGHT:
+				pAngle += M_PI / 36;
+				break;
+			case KEY_LEFT:
+				pAngle -= M_PI / 36;
+				break;
+			case '1':
+				fov += M_PI / 90;
+				break;
+			case '2':
+				fov -= M_PI / 90;
+				break;
+			default:
+				break;
+		}
+	}
+	endwin();
+	return 0;
+}
+
+void init_raycaster() {
 	initscr();
 	noecho();
 	cbreak();
@@ -39,100 +64,74 @@ int main() {
 	init_pair(5, COLOR_BLUE, COLOR_BLACK);
 	init_pair(6, COLOR_MAGENTA, COLOR_BLACK);
 	init_pair(7, COLOR_CYAN, COLOR_BLACK);
-	// gameloop starts here
-	while(1) {
-		// loop over columns
-		for (int ic = 0; ic < COLS; ic++) {
-			// fire ray
-			rX = pX;
-			rY = pY;
-			rDist = 0;
-			while(1) {
-				rAngle = ((FOV / 2) + pAngle) + (ic * (FOV / COLS));
-				rX += cos(rAngle) * RAY_JUMP_FACTOR;
-				rY += sin(rAngle) * RAY_JUMP_FACTOR;
-				rDist += RAY_JUMP_FACTOR;
-				// check if ray hit a wall
-				wStatus = map[(int) rY][(int) rX];
-				if (wStatus)
+}
+
+void gen_frame() {
+	// used for wall color and presence
+	int wStatus;
+	// loop over columns
+	for (int ic = 0; ic < COLS; ic++) {
+		// fire ray
+		double rX = pX;
+		double rY = pY;
+		double rDist = 0;
+		while(1) {
+			double rAngle = ((fov / 2) + pAngle) + (ic * (fov / COLS));
+			rX += cos(rAngle) * R_JMP_FACTOR;
+			rY += sin(rAngle) * R_JMP_FACTOR;
+			rDist += R_JMP_FACTOR;
+			// check if ray hit a wall
+			wStatus = map[(int) rY][(int) rX];
+			if (wStatus)
+				break;
+		}
+		// create a column
+		// height that the wall appears to the player
+		int h = (int) ( ( 1 / pow(rDist,2) ) * LINES);
+		// get the height that the floor appears
+		// this may be problematic if (LINES - h) is not even
+		int f = ( LINES - h ) / 2;
+		for (int ir = 0; ir < LINES; ir++) {
+			// draw the walls
+			switch (wStatus) {
+				case 2:
+					attron(COLOR_PAIR(2));
+					break;
+				case 3:
+					attron(COLOR_PAIR(3));
+					break;
+				case 4:
+					attron(COLOR_PAIR(4));
+					break;
+				case 5:
+					attron(COLOR_PAIR(5));
+					break;
+				case 6:
+					attron(COLOR_PAIR(6));
+					break;
+				case 7:
+					attron(COLOR_PAIR(7));
+					break;
+				default:
 					break;
 			}
-			// create a column
-			// height that the wall appears to the player
-			int h = (int) ( ( 1 / pow(rDist,2) ) * LINES);
-			// get the height that the floor appears
-			// this may be problematic if (LINES - h) is not even
-			int f = ( LINES - h ) / 2;
-			for (int ir = 0; ir < LINES; ir++) {
-				// draw the walls
-				switch (wStatus) {
-					case 2:
-						attron(COLOR_PAIR(2));
-						break;
-					case 3:
-						attron(COLOR_PAIR(3));
-						break;
-					case 4:
-						attron(COLOR_PAIR(4));
-						break;
-					case 5:
-						attron(COLOR_PAIR(5));
-						break;
-					case 6:
-						attron(COLOR_PAIR(6));
-						break;
-					case 7:
-						attron(COLOR_PAIR(7));
-						break;
-					default:
-						break;
-				}
-				if ((ir < f) || (ir > (f + h))) {
-					mvaddch(ir, ic, ' ');
-				}
-				else if (rDist < 2) {
-					attron(A_STANDOUT);
-					mvaddch(ir, ic, ' ');
-					attroff(A_STANDOUT);
-				}
-				else if (rDist < 4)
-					mvaddch(ir, ic, WALL_DARK);
-				else {
-					attron(COLOR_PAIR(1));
-					mvaddch(ir, ic, WALL_DARKER);
-				}
-				attron(COLOR_PAIR(1));
+			if ((ir < f) || (ir > (f + h))) {
+				mvaddch(ir, ic, ' ');
 			}
-		}
-		// update coords
-		mvprintw(0, 0, "%d.%d, %d.%d", (int) pX, (int) (pX * 10) % 10, (int) pY, (int) (pY * 10) % 10);
-		// print the frame
-		refresh();
-		// get input
-		switch (getch()) {
-			case KEY_UP:
-				// collision checking
-				if (!map[(int) (pY + sin(pAngle+0.5*M_PI) * PLAYER_MOVE_FACTOR)][(int) (pX + cos(pAngle+0.5*M_PI) * PLAYER_MOVE_FACTOR)]) {
-					pX += cos(pAngle+0.5*M_PI) * PLAYER_MOVE_FACTOR;
-					pY += sin(pAngle+0.5*M_PI) * PLAYER_MOVE_FACTOR;
-				}
-				break;
-			case KEY_DOWN:
-				if (!map[(int) (pY - sin(pAngle+0.5*M_PI) * PLAYER_MOVE_FACTOR)][(int) (pX - cos(pAngle+0.5*M_PI) * PLAYER_MOVE_FACTOR)]) {
-					pX -= cos(pAngle+0.5*M_PI) * PLAYER_MOVE_FACTOR;
-					pY -= sin(pAngle+0.5*M_PI) * PLAYER_MOVE_FACTOR;
-				}
-				break;
-			case KEY_RIGHT:
-				pAngle += M_PI / 36;
-				break;
-			case KEY_LEFT:
-				pAngle -= M_PI / 36;
-				break;
-			default:
-				break;
+			else if (rDist < 2) {
+				attron(A_STANDOUT);
+				mvaddch(ir, ic, ' ');
+				attroff(A_STANDOUT);
+			}
+			else if (rDist < 4)
+				mvaddch(ir, ic, W_DARK);
+			else {
+				attron(COLOR_PAIR(1));
+				mvaddch(ir, ic, W_DARKER);
+			}
+			attron(COLOR_PAIR(1));
 		}
 	}
-	endwin();
-	return 0;
+	// update coords
+	mvprintw(0, 0, "%d.%d, %d.%d", (int) pX, (int) (pX * 10) % 10, (int) pY, (int) (pY * 10) % 10);
 }
---
+++ main.h
@@ -0,0 +1,27 @@
+#ifndef _MAIN_
+#define _MAIN_
+
+#define MAPLENGTH 10
+#define R_JMP_FACTOR 0.1
+#define W_DARK '#'
+#define W_DARKER ':'
+#define P_MV_FACTOR 0.1
+
+const int map[][MAPLENGTH] = {
+		{1,1,2,1,3,1,4,1,5,1},
+		{1,0,0,0,0,0,0,0,0,1},
+		{1,0,0,0,0,0,0,0,0,6},
+		{1,0,0,0,0,0,0,0,0,1},
+		{1,0,0,0,0,0,0,0,0,7},
+		{1,0,0,0,0,0,0,0,0,1},
+		{1,0,0,0,0,0,0,0,0,1},
+		{1,0,0,0,0,0,0,0,0,1},
+		{1,0,0,0,0,0,0,0,0,1},
+		{1,1,1,1,1,1,1,1,1,1}
+};
+
+void init_raycaster();
+
+void gen_frame();
+
+#endif
