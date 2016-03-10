69ef43e8334ce266b307621d1ad2f54e50872bcd
io12 <io12@users.noreply.github.com>
2016-03-09 21:30:09 -0500

    removed a comment

--- src/ncaster.h
+++ src/ncaster.h
@@ -1,10 +1,6 @@
 #ifndef _NCASTER_
 #define _NCASTER_
 
-// curses.h must be included before this
-// this probably works with stdbool.h
-// will test it later
-
 #define FOV_DEFAULT 2
 
 struct player {
